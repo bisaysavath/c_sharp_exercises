219dfbf2de8206c8fd6528a9e7b6294ca501984b
Billy Saysavath <bisaysavath@hotmail.com>
2017-12-11 08:11:04 -0800

    First commit

---
+++ Chapter01/WelcomeDotNetCore/src/WelcomeDotNetCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WelcomeDotNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome, >NET Core!");
+        }
+    }
+}
