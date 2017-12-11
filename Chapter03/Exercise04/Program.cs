46605e91108b141be6af1194ec58417644540562
Billy Saysavath <bisaysavath@hotmail.com>
2017-12-11 15:12:12 -0800

    Finish Chapter 03

---
+++ Chapter03/Exercise04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            try
+            {
+                Write("Enter a number between 0 and 255: ");
+                firstNumber = int.Parse(ReadLine());
+                Write("Enter another number between 0 and 255: ");
+                secondNumber = int.Parse(ReadLine());
+
+                WriteLine($"{firstNumber} divided by {secondNumber} is {firstNumber / secondNumber}");
+            }
+            catch (FormatException)
+            {
+                WriteLine("Input string was not in a correct format.");
+            }
+            catch (DivideByZeroException)
+            {
+                WriteLine("It's impossible to divide by a zero.");
+            }
+        }
+    }
+}
---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 0;
+            //string password = string.Empty;
+            //do
+            //{
+            //    Write("Enter your password: ");
+            //    password = ReadLine();
+            //    x++;
+            //} while (password != "secret" && x < 10);
+
+            //if (password == "secret")
+            //{
+            //    WriteLine("Correct!");
+            //}
+
+            //if (x == 10)
+            //{
+            //    WriteLine("Your account has been locked!");
+            //}
+
+            //for (int y = 1; y <= 10; y++)
+            //{
+            //    WriteLine(y);
+            //}
+
+            //string[] names = { "Adam", "Barry", "Charlie" };
+            //foreach (string name in names)
+            //{
+            //    WriteLine($"{name} has {name.Length} characters.");
+            //}
+
+            //IEnumerator e = names.GetEnumerator();
+            //while (e.MoveNext())
+            //{
+            //    string name = (string)e.Current; // Current is read-only!
+            //    WriteLine($"{name} has {name.Length} characters.");
+            //}
+        }
+    }
+}
