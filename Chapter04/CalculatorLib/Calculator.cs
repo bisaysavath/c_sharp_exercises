d8d401d3dee242f56acde7e86b948af7e8285eb8
Billy Saysavath <bisaysavath@hotmail.com>
2017-12-13 16:28:40 -0800

    Finish Chapter04

---
+++ Chapter04/CalculatorLib/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.CS7
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
