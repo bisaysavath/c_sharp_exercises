777aa33f97737569588f3b5c7635c15edd24f278
Billy Saysavath <bisaysavath@hotmail.com>
2017-12-15 16:28:15 -0800

    Gone halfway through Chapter 05

---
+++ Chapter05/PacktLibrary/Person2.cs
@@ -0,0 +1,17 @@
+namespace Packt.CS7
+{
+    public partial class Person
+    {
+        public string Origin
+        {
+            get
+            {
+                return $"{Name} was born on {HomePlanet}";
+            }
+        }
+
+        public string Greeting => $"{Name} says 'Hello!'";
+
+        public int Age => (int)(System.DateTime.Today.Subtract(DateOfBirth).TotalDays / 365.25);
+    }
+}
