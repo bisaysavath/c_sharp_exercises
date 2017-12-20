5c5026dc81128b97d02f0970308196dff04369fe
Billy Saysavath <bisaysavath@hotmail.com>
2017-12-20 12:00:11 -0800

    .gitignore is now working

--- Chapter05/PacktLibrary/BankAccount.cs
+++
@@ -1,9 +0,0 @@
-namespace Packt.CS7
-{
-    public class BankAccount
-    {
-        public string AccountName;
-        public decimal Balance;
-        public static decimal InterestRate;
-    }
-}
