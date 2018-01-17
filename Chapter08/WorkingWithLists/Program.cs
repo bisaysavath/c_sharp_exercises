09f3e804b86c1445a8d773ab7e2f7afb29d53adf
Billy Saysavath <bisaysavath@hotmail.com>
2018-01-16 16:33:54 -0800

    Continue Cahpter 08

---
+++ Chapter08/WorkingWithLists/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Immutable;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace WorkingWithLists
+{
+   class Program
+    {
+        static void Main()
+        {
+            var cities = new List<string>();
+            cities.Add("London");
+            cities.Add("Paris");
+            cities.Add("Milan");
+            WriteLine("Initial list");
+            foreach (string city in cities)
+            {
+                WriteLine($" {city}");
+            }
+
+            WriteLine($"The first city is {cities[0]}.");
+            WriteLine($"The last city is {cities[cities.Count - 1]}.");
+            cities.Insert(0, "Sydney");
+            WriteLine("After inserting Sydney at index 0");
+            foreach (string city in cities)
+            {
+                WriteLine($" {city}");
+            }
+            cities.RemoveAt(1);
+            cities.Remove("Milan");
+            WriteLine("After removing two cities");
+            foreach (string city in cities)
+            {
+                WriteLine($" {city}");
+            }
+
+            var immutableCites = cities.ToImmutableList();
+            var newList = immutableCites.Add("Rio");
+
+            Write("Immutable cities:");
+            foreach (string city  in immutableCites)
+            {
+                Write($" {city}");
+            }
+            WriteLine();
+
+            Write("New cities:");
+            foreach (string city in newList)
+            {
+                Write($" {city}");
+            }
+            WriteLine();
+        }
+    }
+}
--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace WorkingWithText
 {
@@ -9,6 +10,40 @@ namespace WorkingWithText
             string city = "London";
             WriteLine($"{city} is {city.Length} characters long.");
             WriteLine($"First char is {city[0]} and third is {city[2]}.");
+
+            string cities = "Paris,Berlin,Madrid,New York";
+            string[] citiesArray = cities.Split(',');
+            foreach (var item in citiesArray)
+            {
+                WriteLine(item);
+            }
+
+            string fullname = "Alan Jones";
+            int indexOfTheSpace = fullname.IndexOf(' ');
+            string firstname = fullname.Substring(0, indexOfTheSpace);
+            string lastname = fullname.Substring(indexOfTheSpace + 1);
+            WriteLine($"{lastname}, {firstname}");
+
+            string fullname2 = "Jones, Alan";
+            firstname = fullname2.Substring(fullname2.IndexOf(' ') + 1);
+            lastname = fullname2.Substring(0, fullname2.IndexOf(','));
+            WriteLine($"{firstname} {lastname}");
+
+            string company = "Microsoft";
+            bool startsWithM = company.StartsWith('M');
+            bool containN = company.Contains("N");
+            WriteLine($"Starts with M: {startsWithM}, contains an N: {containN}");
+
+            string recombined = string.Join(" => ", citiesArray);
+            WriteLine(recombined);
+
+            string fruit = "Apples";
+            decimal price = 0.39M;
+            DateTime when = DateTime.Today;
+
+            WriteLine($"{fruit} cost {price:C} on {when:dddd}s.");
+
+            WriteLine(string.Format("{0} cost {1:C} on {2:dddd}s.", fruit, price, when));
         }
     }
 }
