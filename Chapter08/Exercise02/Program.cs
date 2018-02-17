b0e49ad0b5a1597243cebfd0b1cb83e7994be426
Billy Saysavath <bisaysavath@hotmail.com>
2018-02-16 16:28:03 -0800

    Start Chapter09

---
+++ Chapter08/Exercise02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using System;
+using static System.Console;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("The default regular expression checks for at least one digit.");
+            ConsoleKeyInfo info;
+
+            do
+            {
+                Write("Enter a regular expression (or press ENTER to use the default): ");
+                string regInput = ReadLine();
+                Write("Enter some input: ");
+                string input = ReadLine();
+                var regChecker = new Regex(@regInput);
+                WriteLine($"{input} mathces {regInput}? {regChecker.IsMatch(input)}");
+                WriteLine("Press ESC to end or any key to try again.");
+                info = ReadKey();
+            } while (info.Key != ConsoleKey.Escape);
+        }
+    }
+}
