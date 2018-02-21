5a41be59b7addb09391c56efbe0cc6ba028458eb
Billy Saysavath <bisaysavath@hotmail.com>
2018-02-20 16:25:36 -0800

    Finish Chapter10

---
+++ Chapter10/EncryptionApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using Packt.CS7;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a message that you want to encrypt: ");
+            string message = ReadLine();
+            Write("Enter a password: ");
+            string password = ReadLine();
+            string cryptoText = Protector.Encrypt(message, password);
+            WriteLine($"Encrypted text: {cryptoText}");
+            Write("Enter the password: ");
+            string password2 = ReadLine();
+            try
+            {
+                string clearText = Protector.Decrypt(cryptoText, password2);
+                WriteLine($"Decrypted text: {clearText}");
+            }
+            catch (System.Exception)
+            {
+                WriteLine("Unable to decrypt because you entered the wrong password.");
+            }
+        }
+    }
+}
---
+++ Chapter10/Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+using System.IO;
+using Packt.CS7;
+using System.Xml;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmlStr = File.ReadAllText("UserInfo.xml");
+
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlStr)))
+            {
+                while (reader.Read())
+                {
+                   switch (reader.NodeType)
+                   {
+                    case XmlNodeType.Element:
+                        WriteLine($"Element: {reader.Name}");
+                        break;
+                    case XmlNodeType.Text:
+                        var secret = Protector.Encrypt(reader.Value, "ABC");
+                        WriteLine($"Text: {secret}");
+                        WriteLine($"Text: {Protector.Decrypt(secret, "ABC")}");
+                        break;
+                   }
+                }
+            }
+        }
+    }
+}
