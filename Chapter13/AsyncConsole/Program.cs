7d682d5144da6636f8cb762bbcd24e76f2bd8645
Billy Saysavath <bisaysavath@hotmail.com>
2018-04-02 16:28:40 -0700

    Finished Chapter 13

---
+++ Chapter13/AsyncConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncConsole
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://www.apple.com/");
+            WriteLine($"Apple's homepage has {response.Content.Headers.ContentLength:N0} bytes.");
+            ReadLine();
+        }
+    }
+}
--- Chapter13/WorkingWithTasks/Program.cs
+++ Chapter13/WorkingWithTasks/Program.cs
@@ -29,6 +29,22 @@ namespace WorkingWithTasks
             WriteLine("Finished Method C.");
         }
 
+        static decimal CallWebService()
+        {
+            WriteLine("Starting call to web service...");
+            Thread.Sleep((new Random()).Next(2000, 4000));
+            WriteLine("Finished call to web service.");
+            return 89.99M;
+        }
+
+        static string CallStoredProcedure(decimal amount)
+        {
+            WriteLine("Starting call to stored procedure...");
+            Thread.Sleep((new Random()).Next(2000, 4000));
+            WriteLine("Finished call to stored procedure.");
+            return $"12 products cost more than {amount:C}.";
+        }
+
         public static void Main()
         {
             var timer = Stopwatch.StartNew();
@@ -38,14 +54,20 @@ namespace WorkingWithTasks
             //MethodB();
             //MethodC();
 
-            WriteLine("Running methods synchronously on one thread.");
-            Task taskA = new Task(MethodA);
-            taskA.Start();
-            Task taskB = Task.Factory.StartNew(MethodB);
-            Task taskC = Task.Run(new Action(MethodC));
+            //WriteLine("Running methods synchronously on one thread.");
+            //Task taskA = new Task(MethodA);
+            //taskA.Start();
+            //Task taskB = Task.Factory.StartNew(MethodB);
+            //Task taskC = Task.Run(new Action(MethodC));
 
-            Task[] tasks = { taskA, taskB, taskC };
-            Task.WaitAll(tasks);
+            //Task[] tasks = { taskA, taskB, taskC };
+            //Task.WaitAll(tasks);
+
+            WriteLine("Passing the result of one task as an input into another.");
+
+            var taskCallWebServiceAndThenStoredProcedure = Task.Factory.StartNew(CallWebService).ContinueWith(previousTask => CallStoredProcedure(previousTask.Result));
+
+            WriteLine($"{taskCallWebServiceAndThenStoredProcedure.Result}");
 
             WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
 
